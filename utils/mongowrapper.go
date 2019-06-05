4db4d852df2f368c40441f4ea294286924a81d27
lchan <leochan007@163.com>
2019-06-05 10:44:52 +0800

    update for new mongodb driver.

--- utils/mongowrapper.go
+++ utils/mongowrapper.go
@@ -3,8 +3,9 @@ package utils
 import (
 	"context"
 	"fmt"
-	"github.com/mongodb/mongo-go-driver/bson"
-	"github.com/mongodb/mongo-go-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"log"
 	"strings"
 	"time"
@@ -23,7 +24,7 @@ func (instance *MongoWrapper) InitClient(mongoUrl string, dbName string, collect
 		tmpMongoUrl += "/"
 	}
 	url := tmpMongoUrl + dbName + mongoOpt
-	instance.client, err = mongo.Connect(instance.ctx, url)
+	instance.client, err = mongo.Connect(instance.ctx, options.Client().ApplyURI(url))
 	if err == nil {
 		instance.collection = instance.client.Database(dbName).Collection(collectionName)
 	}
